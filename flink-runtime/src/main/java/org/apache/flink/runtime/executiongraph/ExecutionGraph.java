ccc186ad9b9a7e8a47ea87416e0d496d0450141a
Gary Yao <gary@apache.org>
2019-06-14 12:57:33 +0200

    [hotfix][runtime] Extract method cancelVerticesAsync() in ExecutionGraph

--- flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/ExecutionGraph.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/ExecutionGraph.java
@@ -1088,15 +1088,7 @@ public class ExecutionGraph implements AccessExecutionGraph {
 						ongoingSchedulingFuture.cancel(false);
 					}
 
-					final ArrayList<CompletableFuture<?>> futures = new ArrayList<>(verticesInCreationOrder.size());
-
-					// cancel all tasks (that still need cancelling)
-					for (ExecutionJobVertex ejv : verticesInCreationOrder) {
-						futures.add(ejv.cancelWithFuture());
-					}
-
-					// we build a future that is complete once all vertices have reached a terminal state
-					final ConjunctFuture<Void> allTerminal = FutureUtils.waitForAll(futures);
+					final ConjunctFuture<Void> allTerminal = cancelVerticesAsync();
 					allTerminal.whenComplete(
 						(Void value, Throwable throwable) -> {
 							if (throwable != null) {
@@ -1140,6 +1132,18 @@ public class ExecutionGraph implements AccessExecutionGraph {
 				return;
 			}
 		}
+	}
+
+	private ConjunctFuture<Void> cancelVerticesAsync() {
+		final ArrayList<CompletableFuture<?>> futures = new ArrayList<>(verticesInCreationOrder.size());
+
+		// cancel all tasks (that still need cancelling)
+		for (ExecutionJobVertex ejv : verticesInCreationOrder) {
+			futures.add(ejv.cancelWithFuture());
+		}
+
+		// we build a future that is complete once all vertices have reached a terminal state
+		return FutureUtils.waitForAll(futures);
 	}
 
 	/**
@@ -1230,14 +1234,7 @@ public class ExecutionGraph implements AccessExecutionGraph {
 				}
 
 				// we build a future that is complete once all vertices have reached a terminal state
-				final ArrayList<CompletableFuture<?>> futures = new ArrayList<>(verticesInCreationOrder.size());
-
-				// cancel all tasks (that still need cancelling)
-				for (ExecutionJobVertex ejv : verticesInCreationOrder) {
-					futures.add(ejv.cancelWithFuture());
-				}
-
-				final ConjunctFuture<Void> allTerminal = FutureUtils.waitForAll(futures);
+				final ConjunctFuture<Void> allTerminal = cancelVerticesAsync();
 				FutureUtils.assertNoException(allTerminal.handle(
 					(Void ignored, Throwable throwable) -> {
 						if (throwable != null) {
